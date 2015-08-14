6d89b06a243f0eae9c2ea8ea55d70e02739fd88c
candle <7157959+cndlhvn@users.noreply.github.com>
2015-08-14 15:19:39 +0900

    create config file

--- lib/get_link_list/engine.rb
+++ lib/get_link_list/engine.rb
@@ -1,5 +1,14 @@
 require 'get_link_list/helper'
+require 'get_link_list/config'
 module GetLinkList
+  def self.config
+    @config ||= GetLinkList::Config.new
+  end
+
+  def self.configure(&block)
+    yield(config) if block_given?
+  end
+
   class Engine < ::Rails::Engine
     initializer 'get_link_list.action_view_helpers' do
       ActiveSupport.on_load :action_view do
--- lib/get_link_list/helper.rb
+++ lib/get_link_list/helper.rb
@@ -4,8 +4,10 @@ module GetLinkList
     def render_get_links
       @paths = Hash.new { |hash,key| hash[key] = Hash.new {} }
       Rails.application.routes.routes.each do |route|
-        if "GET" =~ route.constraints[:request_method] && route.path.spec.left != "/"
-          @paths[route.defaults[:controller]][route.defaults[:action]] = match_initial_path_segment(route.path.spec.to_s)
+        if "GET" =~ route.constraints[:request_method] && route.path.spec.left != "/" 
+          if not GetLinkList.config.ignore_controller.include?(route.defaults[:controller])
+            @paths[route.defaults[:controller]][route.defaults[:action]] = match_initial_path_segment(route.path.spec.to_s)
+          end
         end
       end
       render partial: 'get_link_list/get_link', locals: { paths: @paths  }
